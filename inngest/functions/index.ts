0bcdafb38691421c665ff7c354148513afb6911a
Tanmay Singh <tanmaysingh7706@gmail.com>
2026-07-22 21:56:50 +0530

    chore: remove deleted inngest files to fix vercel build

--- inngest/functions/index.ts
+++
@@ -1,5 +0,0 @@
-export { classifyTranscriptChunk } from "./classifyTranscriptChunk";
-export { enrichEntityGraph } from "./enrichEntityGraph";
-export { checkConvergence } from "./checkConvergence";
-export { orchestrateAlertResponse } from "./orchestrateAlertResponse";
-export { playDemoScenario } from "./playDemoScenario";
